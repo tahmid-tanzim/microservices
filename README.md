63e234f58967e2f78e9b07f5eb77ae8845a49980
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-10-09 20:14:26 +0600

    Initial repository setup

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# MEAN Retail
-An Online Shop builds with MongoDB, Express.js, AngularJS, Node.js
+# Microservices
--- fx.js
+++
@@ -1,7 +0,0 @@
-modules.exports = function() {
-    return {
-        USD: 1,
-        EUR: 1.1,
-        GBP: 1.5
-    };
-};

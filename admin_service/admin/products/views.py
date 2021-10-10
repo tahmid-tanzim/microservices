69d75cc20562ba3c5239531cd3103f7eb9b821ed
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-10-10 18:19:59 +0600

    Rest API retrieve, update & remove

--- admin_service/admin/products/views.py
+++ admin_service/admin/products/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, response, status
+# from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -7,20 +9,36 @@ class ProductViewSet(viewsets.ViewSet):
     def list(self, request):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
-        return response.Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def create(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
-        pass
+        try:
+            product = Product.objects.get(id=pk)
+            serializer = ProductSerializer(product, many=False)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Product.DoesNotExist:
+            return Response({"message": "Sorry! Product DoesNotExist"}, status=status.HTTP_404_NOT_FOUND)
 
     def update(self, request, pk=None):
-        pass
+        try:
+            product = Product.objects.get(id=pk)
+            serializer = ProductSerializer(instance=product, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        except Product.DoesNotExist:
+            return Response({"message": "Sorry! Product DoesNotExist"}, status=status.HTTP_404_NOT_FOUND)
 
     def remove(self, request, pk=None):
-        pass
-
+        try:
+            product = Product.objects.get(id=pk)
+            product.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Product.DoesNotExist:
+            return Response({"message": "Sorry! Product DoesNotExist"}, status=status.HTTP_404_NOT_FOUND)

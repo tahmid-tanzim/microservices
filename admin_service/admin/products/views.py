858a613dac1b35899609e1e84534a41c4e861498
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-10-10 16:56:57 +0600

    Rest API with Models, Serializers & ViewSets

--- admin_service/admin/products/views.py
+++ admin_service/admin/products/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import viewsets, response, status
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return response.Response(serializer.data)
+
+    def create(self, request):
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def remove(self, request, pk=None):
+        pass
+
